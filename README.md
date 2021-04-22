a24210813fc7695325a4b0c42a88d227a4c63ee4
Carl Winkler <carl@winkler.id.au>
2021-04-22 14:11:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# frisker
+
+> Quick and declarative object validation with TypeScript with simple objects
+
+## Why?
+
+When building APIs I will need to validate the `request.body` or `request.query` object to ensure it conforms to a type.  
+I have done this in the past with varying amount of rigor, robustness, and verbosity.
+
+I wanted a clear, yet terse way to solve this problem that was still robust and not overly repetitive.
+
+## Requirements
+
+TypeScript 4+
+
+## Installation
+
+```sh
+> yarn add frisker
+```
+
+## Usage
+
+Frisker can check for a complete or partial match of an object to a validator object.  
+Validator objects are real objects declared using the `as const` syntax that closely resembles a TypeScript type or interface.  
+The objects can be deeply nested and composed together using the object spread syntax.
+
+Objects are comprised the values:
+
+- Primitives
+  - `'string'`
+  - `'number'`
+  - `'boolean'`
+- A single element tuple with a primitive or an object which represents an array
+  - `[Primitive] | [Object]`
+- Objects containing all of the previous types of values
+- `{ [key: string]: Primtive | [Primitive] | [Object] | Object }`
+
+This is a fairly shallow example using all of the available "types"
+
+```ts
+export const user = {
+  id: 'string',
+  name: { first: 'string', last: 'string', alias: 'string' },
+  age: 'number',
+  isAdmin: 'boolean',
+  permissions: ['string'],
+  groups: [{ id: 'string', name: 'string' }],
+} as const
+
+export type User = UnwrapBody<typeof user>
+```
+
+### Type Guard
+
+```ts
+function isValid(type: Validator, input: any)
+```
+
+Frisker can be used as a **Type Guard**:
+
+```ts
+// Example using an express handler:
+import { Request, Response } from 'express'
+import { isValid } from 'frisker'
+import { user } from './types'
+
+export function ({ body }: Request, res: Response) {
+  if (!isValid(user, body)) {
+    return res.status(400).json({ message: 'Bad request' })
+  }
+
+  // body is now a User type which is inferred from the first parameter is isValid()
+  if (!body.permissions.includes('users')) {
+    return res.status(403).json({ message: 'Not allowed' })
+  }
+
+  return res.json({ message: 'ok' })
+}
+```
+
+### Partial Type Guard
+
+```ts
+function isValidPartial(type: Validator, input: any)
+```
+
+```ts
+import { isValidPartial } from 'frisker'
+import { user } from './types' // From the previous example
+
+export function handler({ body, params }: Request, res: Response) {
+  if (!isValidPartial(user.name, body)) {
+    return res.status(400).json({ message: 'Bad request' })
+  }
+
+  await updateName(params.id, body)
+  res.json({ message: 'ok' })
+}
+```
+
+### Configurable Validation
+
+```ts
+function validateBody(type: Validator, input: any, options: Options)
+```
+
+Using the `validateBody()` function you can get more detail information about the validation
+
+**Options**
+
+- `notThrow: boolean`: Defaults to `false`
+  - the `validateBody` function throws by default with a message containing all of the errors
+  - Provide `true` if you want an array of the errors
+- `partial: boolean`
+  - Will only validate properties provided in the object provided for comparison
---
+++ src/index.ts
@@ -0,0 +1,158 @@
+type Primitive = 'string' | 'number' | 'boolean'
+type Reference =
+  | Primitive
+  | readonly [Primitive]
+  | [Primitive]
+  | readonly [Validator]
+  | [Validator]
+  | Validator
+type Validator = { [key: string]: Reference }
+
+type FromPrimitve<T extends Primitive> = T extends 'string'
+  ? string
+  : T extends 'boolean'
+  ? boolean
+  : T extends 'number'
+  ? number
+  : never
+
+type FromTuple<T> = T extends [infer U] | readonly [infer U]
+  ? U extends Primitive
+    ? Array<FromPrimitve<U>>
+    : never
+  : never
+
+type FromTupleBody<T> = T extends [infer U]
+  ? U extends Validator
+    ? Array<UnwrapBody<U>>
+    : never
+  : T extends readonly [infer U]
+  ? U extends Validator
+    ? Array<UnwrapBody<U>>
+    : never
+  : never
+
+export type UnwrapBody<T extends { [key: string]: Reference }> = {
+  -readonly [key in keyof T]: T[key] extends Primitive
+    ? FromPrimitve<T[key]>
+    : T[key] extends [Primitive] | readonly [Primitive]
+    ? FromTuple<T[key]>
+    : T[key] extends [Validator] | readonly [Validator]
+    ? FromTupleBody<T[key]>
+    : T[key] extends Validator
+    ? UnwrapBody<T[key]>
+    : never
+}
+
+export function isValid<T extends Validator>(type: T, compare: any): compare is UnwrapBody<T> {
+  const errors = validateBody(type, compare, { notThrow: true })
+  return errors.length === 0
+}
+
+export function isValidPartial<T extends Validator>(
+  type: T,
+  compare: any
+): compare is Partial<UnwrapBody<T>> {
+  const errors = validateBody(type, compare, { notThrow: true, partial: true })
+  return errors.length === 0
+}
+
+export function validateBody<T extends Validator>(
+  type: T,
+  compare: any,
+  opts: { partial?: boolean; prefix?: string; notThrow?: boolean } = {}
+) {
+  const prefix = opts.prefix ? `${opts.prefix}.` : ''
+  const errors: string[] = []
+
+  start: for (const key in type) {
+    const prop = `${prefix}${key}`
+    const bodyType = type[key]
+    let value
+    try {
+      value = compare[key]
+    } catch (ex) {
+      throw new Error(`${ex.message}: ${prop}`)
+    }
+
+    if (value === undefined) {
+      if (!opts.partial) errors.push(`${prop} is undefined`)
+      continue
+    }
+
+    if (isPrimitive(bodyType) && typeof value !== bodyType) {
+      errors.push(`${prop} is ${typeof value}, expected ${bodyType}`)
+      continue
+    }
+
+    if (isTuplePrimitive(bodyType)) {
+      const [innerType] = bodyType
+      if (!Array.isArray(value)) {
+        errors.push(`${prop} is ${typeof value}, expected Array<${innerType}>`)
+        continue start
+      }
+
+      for (const tupleValue of value) {
+        if (typeof tupleValue === innerType) continue
+
+        // We will exit on the first mismatch
+        // We could report all distinct erronous types?
+        errors.push(`${prop} element contains ${typeof tupleValue}, expected ${innerType}`)
+        continue start
+      }
+    }
+
+    if (isTupleBody(bodyType)) {
+      if (!Array.isArray(value)) {
+        errors.push(`${prop} is ${typeof value}, expected Array`)
+        continue start
+      }
+
+      const [innerBody] = bodyType
+      for (const tupleValue of value) {
+        const innerErrors = validateBody(innerBody, tupleValue, {
+          prefix: prop,
+          notThrow: true,
+          partial: opts.partial,
+        })
+        if (!innerErrors.length) continue
+        errors.push(...innerErrors)
+        continue start
+      }
+    }
+
+    if (typeof bodyType === 'object') {
+      if (typeof value !== 'object') {
+        errors.push(`${prop} is ${typeof value}, expected object`)
+        continue
+      }
+
+      const innerErrors = validateBody(bodyType as any, value, {
+        prefix: prop,
+        partial: opts.partial,
+        notThrow: true,
+      })
+      errors.push(...innerErrors)
+    }
+  }
+
+  if (!opts.notThrow) {
+    throw new Error(`Object does not match type: ${errors.join(', ')}`)
+  }
+
+  return errors
+}
+
+function isPrimitive(value: any): value is Primitive {
+  return typeof value === 'string'
+}
+
+function isTuplePrimitive(value: any): value is [Primitive] {
+  if (Array.isArray(value) === false) return false
+  return typeof value[0] === 'string'
+}
+
+function isTupleBody(value: any): value is [Validator] | readonly [Validator] {
+  if (Array.isArray(value) === false) return false
+  return typeof value[0] === 'object'
+}
